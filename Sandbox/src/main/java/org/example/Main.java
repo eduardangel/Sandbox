8f1881ae3b653475e7727da0dc92c9fa66732726
Eduard Angelskiy <ed_angel74@mail.ru>
2023-10-20 10:01:52 +0700

    Commit_2010233

--- Sandbox/src/main/java/org/example/Main.java
+++ Sandbox/src/main/java/org/example/Main.java
@@ -7,21 +7,16 @@ public class Main {
         hello("Aleksey");
 
         Square s = new Square(6);
-//        s.l = 5;
-        System.out.println("Площадь квадрата со стороной " + s.l + " = " + area(s));
+//        s.l = 6;
+        System.out.println("Площадь квадрата со стороной " + s.l + " = " + s.area());
 
         Rectangle r = new Rectangle(3,4);
 //        r.a = 3;
 //        r.b = 4;
-        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + area(r));
+        System.out.println("Площадь прямоугольника со сторонами " + r.a + " и " + r.b + " = " + r.area());
     }
     public static void hello(String somebody){
         System.out.println("Hello, " + somebody +"!");
     }
-    public static double area(Square s){
-        return s.l * s.l;
-    }
-    public static double area(Rectangle r){
-        return r.a * r.b;
-    }
+
 }
